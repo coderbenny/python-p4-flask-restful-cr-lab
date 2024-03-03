07d581231256df60619642a900009da7c72814b5
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-03-03 16:04:36 +0300

    done

--- server/app.py
+++ server/app.py
@@ -17,11 +17,51 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+        plants = Plant.query.all()
+        plants_list_dict = [plant.to_dict() for plant in plants]
+        
+        response = make_response(
+            jsonify(plants_list_dict),
+            200
+        )
+        return response
+    
+    def post(self):
+        
+        new_plant = Plant(
+            name=request.json.get('name'),
+            image=request.json.get('image'),
+            price=request.json.get('price')
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        response = make_response(
+            jsonify(new_plant.to_dict()),
+            201
+        )
+        
+        return response
+
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    
+    def get(self,id):
+        plant = Plant.query.filter_by(id=id).first()
         
+        if not plant:
+            return jsonify({"Error":"Plant does not exist"}), 404
+        
+        response =  make_response(jsonify(plant.to_dict()),200)
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
